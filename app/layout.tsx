ba8b8ce103b26dcbf9c2cedc080da2ad9bbf8a4c
abhisheksett <abhisheksett@users.noreply.github.com>
2026-08-15 19:52:42 +0530

    cheers updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import { Fraunces } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistSans = Geist({
